05e514ff1ee87f1e6834f0bc3af63b71aed7270f
nerseszakoyan <nerses@zakoyan.com>
2015-07-15 11:15:40 +0300

    First commit

---
+++ Yay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yay
+//
+//  Created by Nerses Zakoyan on 15.07.15.
+//  Copyright (c) 2015 Nerses Zakoyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
